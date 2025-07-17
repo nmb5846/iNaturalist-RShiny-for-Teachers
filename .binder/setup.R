b3f91ae1d5f022651c7613babbaece310324ea62
nmb5846 <155582681+nmb5846@users.noreply.github.com>
2025-07-17 15:57:05 -0400

    Update setup.R

--- .binder/setup.R
+++ .binder/setup.R
@@ -1,3 +1,15 @@
 # .binder/setup.R
 
 dir.create("data", showWarnings = FALSE)
+
+zip_path <- "data/cdl_data.zip"
+tif_path <- "data/2024_30m_cdls.tif"
+
+if (!file.exists(tif_path)) {
+  download.file(
+    url = "https://www.nass.usda.gov/Research_and_Science/Cropland/Release/datasets/2024_30m_cdls.zip",
+    destfile = zip_path,
+    mode = "wb"
+  )
+  unzip(zip_path, exdir = "data")
+}
